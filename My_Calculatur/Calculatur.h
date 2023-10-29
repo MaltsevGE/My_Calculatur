ae58c8d1f4a31a2ffa7c211dea552855bbb1c6f1
malts <malts@MORTY>
2023-10-29 20:22:14 +0300

    Добавьте файлы проекта.

---
+++ My_Calculatur/Calculatur.h
@@ -0,0 +1,7 @@
+#pragma once
+class Calculatur
+{
+public:
+	double Calculate(double x, char oper, double y);
+};
+
---
+++ My_Calculatur/My_Calculatur.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "Calculatur.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    cout << "Программа Калькулятор, разработчик: Мальцев Глеб Евгеньевич" << endl << endl;
+    cout << "Пожалуйста, введите операцию, которую необходимо выполнить. Формат: a+b | a-b | a*b | a/b" << endl;
+    
+    Calculatur c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+
+        if (oper == '/' && (x == 0 || y== 0))
+        {
+            cout << "Вы попытались разделить на ноль, но увы не прокатило." << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Результат " << "операция " << x << oper << y << " равен: " << result << endl;
+    }
+
+    return 0;
+}
