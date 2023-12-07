8ee8e4d8537dee4eb0970b85ea8b8af692e10b49
Gleb Maltsev <maltsev.4852@gmail.com>
2023-12-07 22:47:46 +0300

    Расширение калькулятора

--- My_Calculatur/My_Calculatur.cpp
+++ My_Calculatur/My_Calculatur.cpp
@@ -5,14 +5,55 @@ using namespace std;
 
 int main()
 {
+    bool status = true;
+    char select;
+
     setlocale(LC_ALL, "Russian");
-    
+
+    cout << "Программа Калькулятор версия 2.0.0.0, разработчик: Мальцев Глеб Евгеньевич" << endl << endl;
+    cout << "Выбирите режим калькулятора:" << endl;
+    cout << "1 - Простые операции" << endl;
+    cout << "2 - Возведение в степень" << endl;
+    cout << "3 - Вычесление корня" << endl;
+    cout << "4 - Решение линейного уровнения" << endl << endl;
+
+    while (status)
+    {
+        while (cin >> select)
+        {
+            if (isdigit(select)) {
+                switch (select)
+                {
+                case '1':
+                    cout << "1" << endl;
+                    break;
+                case '2':
+                    cout << "2" << endl;
+                    break;
+                case '3':
+                    cout << "3" << endl;
+                    break;
+                case '4':
+                    cout << "4" << endl;
+                    break;
+                default:
+                    cout << "Неверный выбор режима, повторите ввод." << endl;
+                    break;
+                }
+            } 
+            else
+            {
+                cout << "Для выбора режима нужно использовать цифры." << endl;
+            }
+        }
+    }
+
     double x = 0.0;
     double y = 0.0;
     double result = 0.0;
     char oper = '+';
 
-    cout << "Программа Калькулятор версия 1.0.0.0, разработчик: Мальцев Глеб Евгеньевич" << endl << endl;
+    
     cout << "Пожалуйста, введите операцию, которую необходимо выполнить. Формат: a+b | a-b | a*b | a/b" << endl;
     
     Calculatur c;
